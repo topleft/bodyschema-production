4d6201cb6f182928d852680027b329414dcc7969
Pete Jeffryes <pete@flowhub.co>
2016-04-03 15:19:42 -0600

    changes for highlands pilates

--- js/main.js
+++ js/main.js
@@ -15,22 +15,22 @@ var infowindow;
 
 function init() {
   map = new google.maps.Map(document.getElementById("map"),{
-		      center: {lat:39.7385188,lng:-104.9514281},
+		      center: {lat: 39.7619471, lng: -105.0377847},
 		      zoom:12
 		});
-		  
-  var styledMap = new google.maps.StyledMapType(styles, {name: "Styled Map"}); 
+
+  var styledMap = new google.maps.StyledMapType(styles, {name: "Styled Map"});
 
   map.mapTypes.set('map_style', styledMap);
   map.setMapTypeId('map_style');
 
   marker = new google.maps.Marker({
-    position: {lat:39.7385188, lng:-104.9514281},
+    position: {lat: 39.7619471, lng: -105.0377847},
     map: map
   });
 
   infowindow = new google.maps.InfoWindow({
-    content: "<div><a href='https://goo.gl/maps/FsiZJUyFGmT2'><h4>Bodyschema Movement</h4><p>3015 E 14th Ave, Denver, CO 80206, USA</p></a><a href="+'tel:+15102891955'+">(510) 289-1955</a></div>"
+    content: "<div><a href='https://goo.gl/maps/MnsY4zfPnqv'><h4>Bodyschema Movement @ Highlands Pilates</h4><p>3630 W 32nd Ave #2, Denver, CO 80211</p></a><a href="+'tel:+15102894464'+">(510) 289-4464</a></div>"
   });
 
   marker.addListener('click', function() {
@@ -81,8 +81,8 @@ $(".sidebar-nav").on("click", "li", function(){
 });
 
 var showHide = function(elem, elemClass) {
-	elem = $(elem); 
-	elemClass = "."+elemClass 
+	elem = $(elem);
+	elemClass = "."+elemClass
 	if (elem.css('display') === "none"){
 		$(elemClass).slideUp();
 		elem.slideDown();
@@ -92,7 +92,7 @@ var showHide = function(elem, elemClass) {
 };
 
 // **** smooth scroll ******* //
-	
+
   var scroll = true;
   $("#contact").on("click", function(e){
   	if (scroll){
